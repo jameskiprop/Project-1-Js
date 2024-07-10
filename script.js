a7bcb7ee3033cf8e04381f09b26f72f5685c2a1a
jameskiprop <jameskiprop01@gmail.com>
2024-07-10 21:33:37 +0300

    added a new function

--- script.js
+++ script.js
@@ -1,26 +1,56 @@
-// let twopointers = 2;
-// let threepointers = 3;
-// let points = twopointers * 2 + threepointers * 3;
-// console.log(points);
+// // let twopointers = 2;
+// // let threepointers = 3;
+// // let points = twopointers * 2 + threepointers * 3;
+// // console.log(points);
 
-//write a function that prints out all the leap years in the 20th century
+// //write a function that prints out all the leap years in the 20th century
 
-const years = [...Array(100).keys()].map((n) => n + 1900);
-// console.log(years);
-function leapYears(years) {
-  for (let i = 0; i < years.length; i++) {
-    if (years[i] % 4 === 0) {
-      console.log(years[i] + " is a Leap year");
-    }
-  }
-}
-leapYears(years);
+// // function leapYears() {
+// //   for (let year = 1900; year <= 1999; year++) {
+// //     if (year % 4 === 0) {
+// //       console.log(year);
+// //     }
+// //   }
+// // }
+// // leapYears();
 
-// function leapYears() {
-//   for (let year = 1900; year <= 1999; year++) {
-//     if (year % 4 === 0) {
-//       console.log(year);
+// // Another method
+// const years = [...Array(100).keys()].map((n) => n + 1900); // Creating an array of years from 1900 to 1999.
+// // Defining a function to find and print leap years.
+// function leapYears(years) {
+//   // Looping through each year in the array.
+//   for (let i = 0; i < years.length; i++) {
+//     if (years[i] % 4 === 0) {
+//       // Checking if the year is divisible by 4 (a leap year).
+//       console.log(years[i] + " is a Leap year");
 //     }
 //   }
 // }
-// leapYears();
+// leapYears(years);
+
+// /*Given the age of a person who was born on February 29th, write a function that returns how many times the person
+// has celebrated their birthday on the date that they were born.*/
+
+// let age = 38;
+// let currentYear = 2024;
+// let birthYear = 1986;
+
+// function birthdayNumber() {
+//   let times = 0;
+//   for (birthYear; birthYear < currentYear; birthYear++) {
+//     if (birthYear % 4 === 0) {
+//       times++;
+//     }
+//   }
+//   console.log(`${times} times`);
+
+//   return times;
+// }
+// birthdayNumber();
+
+let twoPointers = 5;
+let threePointers = 6;
+function points(twoPointers, threePointers) {
+  return twoPointers * 2 + threePointers * 3;
+}
+console.log(points(twoPointers, threePointers));
