f9ba11f0c93d29f74bf2816d8358450e8dce6227
jameskiprop <jameskiprop01@gmail.com>
2024-07-17 18:09:22 +0300

    some small practice on functions

---
+++ autobiographical.js
@@ -0,0 +1,46 @@
+/* If a number is autobiographical, then the number's digits describe itself. The first digit is the amount of times the 
+digit 0 appears, the second digit is the amount of times the digit 1 appears, the third digit is the amount of times the 
+digit 2 appears, etc. This repeats for all digits of the number. The number 6210001000 is autobiographical, because it 
+has six 0's, two 1's, one 2, zero 3's, zero 4's, zero 5's, one 6, zero 7's, zero 8's, and zero 9's.
+Create a function that takes an integer n and returns whether or not n is an autobiographical number.
+Examples
+
+// isAutobiographical(6210001000) ➞ true
+// isAutobiographical(12345) ➞ false
+// isAutobiographical(1210) ➞ true
+// one 0, two 1's, one 2, zero 3's
+// isAutobiographical(638) ➞ false
+// isAutobiographical(0) ➞ false
+
+claims no zeroes, however zero is the first digit
+Notes
+Numbers with more than 10 digits should always return false (9 is the highest digit in base 10, so you can't go higher than 9,999,999,999).
+n is always >= 0 and is always an integer.*/
+
+//Converting the integer n to a string to easily access each digit.
+//Checking if the length of the string is greater than 10; if so, return false.
+//Initializing an array to count the occurrences of each digit (0-9).
+//Looping through each digit of the string and update the count array accordingly.
+//Looping through each digit of the string again, checking if the count matches the corresponding digit's value.
+//If all checks pass, return true; otherwise, return false.
+
+/*steps to be followed*/
+//1.declare the function that takes the integer
+//2.convert the integer into a string to access all the digits
+//3.store the values of each digit that was converted into a string(variable)
+//4.inside the function state the requirements of an autobiographical no.()
+//5.initiallise an array to count the occurence of every digit (9)
+//6.use a loop,(length of array)  -will compare  the string to the array created
+//7. (boolean)-if it passes the test returns true ...if not it returns false
+//8.call the function
+
+function isAutobiographical(n) {
+  let intArray = [521000001, 2000123];
+  let string = intArray.toString();
+
+  if (string.length > 10) return false;
+
+  for (let i = 0; i < string.length; i++) {}
+
+  return true;
+}
--- script.js
+++ script.js
@@ -1,59 +1,82 @@
-// let twopointers = 2;
-// let threepointers = 3;
-// let points = twopointers * 2 + threepointers * 3;
-// console.log(points);
+// // let twopointers = 2;
+// // let threepointers = 3;
+// // let points = twopointers * 2 + threepointers * 3;
+// // console.log(points);
 
-//write a function that prints out all the leap years in the 20th century
+// //write a function that prints out all the leap years in the 20th century
 
-/*First method*/
+// /*First method*/
 
-// function leapYears() {
-//   for (let year = 1900; year <= 1999; year++) {
-//     if (year % 4 === 0) {
-//       console.log(year);
+// // function leapYears() {
+// //   for (let year = 1900; year <= 1999; year++) {
+// //     if (year % 4 === 0) {
+// //       console.log(year);
+// //     }
+// //   }
+// // }
+// // leapYears();
+
+// /*Another method*/
+
+// const years = [...Array(100).keys()].map((n) => n + 1900); // Creating an array of years from 1900 to 1999.
+// // Defining a function to find and print leap years.
+// function leapYears(years) {
+//   // Looping through each year in the array.
+//   for (let i = 0; i < years.length; i++) {
+//     if (years[i] % 4 === 0) {
+//       // Checking if the year is divisible by 4 (a leap year).
+//       console.log(years[i] + " is a Leap year");
 //     }
 //   }
 // }
-// leapYears();
+// leapYears(years);
 
-/*Another method*/
+// /*Given the age of a person who was born on February 29th, write a function that returns how many times the person
+// has celebrated their birthday on the date that they were born.*/
 
-const years = [...Array(100).keys()].map((n) => n + 1900); // Creating an array of years from 1900 to 1999.
-// Defining a function to find and print leap years.
-function leapYears(years) {
-  // Looping through each year in the array.
-  for (let i = 0; i < years.length; i++) {
-    if (years[i] % 4 === 0) {
-      // Checking if the year is divisible by 4 (a leap year).
-      console.log(years[i] + " is a Leap year");
-    }
-  }
+// // let age = 38;
+// // let currentYear = 2024;
+// // let birthYear = 1986;
+
+// // function birthdayNumber() {
+// //   let times = 0; // initializig the counter
+// //   for (birthYear; birthYear < currentYear; birthYear++) {
+// //     if (birthYear % 4 === 0) {
+// //       times++;
+// //     }
+// //   }
+// //   console.log(`${times} times`);
+
+// //   return times;
+// // }
+// // birthdayNumber();
+
+// // let hours = 6;
+// // function howManySeconds(hours) {
+// //   let seconds = hours * 3600;
+// //   return seconds;
+// // }
+// // console.log(howManySeconds(hours));
+
+// function printMyName(name) {
+//   console.log(name);
+// }
+// printMyName("kiprop, James, quincy, Wanjiku, Wendy, Sasha, Val");
+
+// Example
+// function reusableFunction() {
+//   console.log("Heyya, World");
+// }
+
+// reusableFunction();
+
+function myName(name) {
+  console.log(name);
 }
-leapYears(years);
+const name1 = "james";
+const name2 = "trance";
+const name3 = "Trey";
 
-/*Given the age of a person who was born on February 29th, write a function that returns how many times the person
-has celebrated their birthday on the date that they were born.*/
-
-let age = 38;
-let currentYear = 2024;
-let birthYear = 1986;
-
-function birthdayNumber() {
-  let times = 0; // initializig the counter
-  for (birthYear; birthYear < currentYear; birthYear++) {
-    if (birthYear % 4 === 0) {
-      times++;
-    }
-  }
-  console.log(`${times} times`);
-
-  return times;
-}
-birthdayNumber();
-
-let hours = 6;
-function howManySeconds(hours) {
-  let seconds = hours * 3600;
-  return seconds;
-}
-console.log(howManySeconds(hours));
+myName(name1);
+myName(name2);
+myName(name3);
